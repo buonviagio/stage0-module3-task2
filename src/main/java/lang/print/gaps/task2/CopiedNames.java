59bb6b6be590ed89d38d2064c52d7e3333617acc
Shapovalov Dmytro <buonviagio@gmail.com>
2023-03-08 12:53:04 +0200

    My second commit for Module3 Task2

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -7,7 +7,12 @@ public class CopiedNames {
         int value = 1000;
         int variableThird = 4000;
         int valueTwo = 3000;
-        int variables = 5000, variablesSecond = 7000;
+        int variables = 5000;
+        int variablesSecond = 7000;
         int valueSecond = 90000;
+        int integ = 100000;
+        int myInteger = 1000000;
+        int myVar = 1234567;
+        int my_integer = 1234567;
     }
 }
